5a0f6c9ec9b4d6c2102d1504858c201265ac1c61
danlhennessy <danh118@hotmail.co.uk>
2023-05-30 03:01:13 +0100

    Unit test for database retrieve

---
+++ tests/test_database_utils.py
@@ -0,0 +1,61 @@
+import unittest
+import mysql.connector
+import test_config
+
+
+def retrieve_database(table: str,
+                      keys: list[str],
+                      connection: mysql.connector.MySQLConnection):
+    cursor = connection.cursor()
+    query = f"SELECT {', '.join(keys)} FROM {table}"
+    cursor.execute(query)
+    return cursor.fetchall()
+
+
+class TestRetrieveDatabase(unittest.TestCase):
+    def setUp(self):
+        self.connection = mysql.connector.connect(
+            host='localhost',
+            user=test_config.DB_USER,
+            password=test_config.DB_PASS,
+            database=test_config.DB_NAME
+        )
+        # Create a test table
+        self.create_test_table()
+
+    def tearDown(self):
+        self.drop_test_table()
+        self.connection.close()
+
+    def create_test_table(self):
+        cursor = self.connection.cursor()
+        cursor.execute(
+            "CREATE TABLE test_table (id INT, name VARCHAR(255), age INT)"
+            )
+
+    def drop_test_table(self):
+        cursor = self.connection.cursor()
+        cursor.execute("DROP TABLE test_table")
+
+    def test_retrieve_database(self):
+        cursor = self.connection.cursor()
+        cursor.execute(
+            "INSERT INTO test_table (id, name, age) VALUES (1, 'John', 25)"
+            )
+        cursor.execute(
+            "INSERT INTO test_table (id, name, age) VALUES (2, 'Jane', 30)"
+            )
+        self.connection.commit()
+
+        result = retrieve_database(
+            "test_table",
+            ["id", "name"],
+            self.connection
+            )
+
+        expected_result = [(1, 'John'), (2, 'Jane')]
+        self.assertEqual(result, expected_result)
+
+
+if __name__ == "__main__":
+    unittest.main()
